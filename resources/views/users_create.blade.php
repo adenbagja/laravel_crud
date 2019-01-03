6e8aeb99260757443ed35d97fe6eed0e7a6f872e
adenbagja <adenbagja45@gmail.com>
2019-01-03 22:13:28 +0700

    a

---
+++ resources/views/users_create.blade.php
@@ -0,0 +1,33 @@
+@extends('base')
+@section('content')
+    <!-- Main Section -->
+    <section class="main-section">
+        <!-- Add Your Content Inside -->
+        <div class="content">
+            <!-- Remove This Before You Start -->
+            <h1>CRUD -  Tambah User</h1>
+            <hr>
+            <form action="{{ route('users.store') }}" method="post">
+                {{ csrf_field() }}
+                <div class="form-group">
+                    <label for="nama">Nama:</label>
+                    <input type="text" class="form-control" id="usr" name="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Password :</label>
+                    <input type="text" class="form-control" id="password" name="password">
+                </div>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-md btn-primary">Submit</button>
+                    <button type="reset" class="btn btn-md btn-danger">Cancel</button>
+                </div>
+            </form>
+        </div>
+        <!-- /.content -->
+    </section>
+    <!-- /.main-section -->
+@endsection
